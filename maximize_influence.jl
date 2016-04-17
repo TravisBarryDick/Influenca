3face136d7b5b35173820965efaa64169d783445
Travis Dick <travis.barry.dick@gmail.com>
2016-04-16 20:57:55 -0400

    Implemented the CELF algorithm for influence maximization.

--- maximize_influence.jl
+++ maximize_influence.jl
@@ -1,3 +1,5 @@
+using Base.Collections
+
 function maximize_influence(s::SimulationState, k; N=20000)
     nodes = Array(Int, 0)
     for iter in 1:k
@@ -15,8 +17,36 @@ function maximize_influence(s::SimulationState, k; N=20000)
                 best_node = i
             end
         end
-        println("Adding node $best_node (Influence = $best_score).")
         push!(nodes, best_node)
+    end
+    return nodes
+end
+
+function maximize_influence_CELF(state::SimulationState, k; N=20000)
+    nodes = Array(Int, 0)
+    current_influence = 0.0
+    valid = zeros(Bool, num_nodes(state.g))
+    q = PriorityQueue(Int, Float64, Base.Order.Reverse)
+    for i in 1:num_nodes(state.g)
+        enqueue!(q, i, Inf)
+    end
+    for iter in 1:k
+        fill!(valid, false)
+        while true
+            s = dequeue!(q)
+            if valid[s]
+                push!(nodes, s)
+                current_influence = evaluate_influence!(state, nodes, N=N)
+                println("Adding $s new influence = $current_influence")
+                break
+            else
+                push!(nodes, s)
+                new_influence = evaluate_influence!(state, nodes, N=N)
+                pop!(nodes)
+                valid[s] = true
+                enqueue!(q, s, new_influence - current_influence)
+            end
+        end
     end
     return nodes
 end
