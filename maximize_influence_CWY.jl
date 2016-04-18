7033bc3c4e2bae4901f2e986541a3e31e466256e
Travis Dick <travis.barry.dick@gmail.com>
2016-04-18 14:52:26 -0400

    Fixed implementation of max_influence_CWY.

--- maximize_influence_CWY.jl
+++ maximize_influence_CWY.jl
@@ -1,64 +1,75 @@
 using Base.Collections
 
-function maximize_influence_CWY(g::Graph, β::Float64, k; N=20000)
-    ccid = Array(Int, num_nodes(g))
-    sizes = Array(Int, num_nodes(g))
-    covered_by_S = Array(Bool, num_nodes(g))
-    visited = Array(Bool, num_nodes(g))
-    node_stack = Array(Int, 0)
-    node_scores = Array(Float64, num_nodes(g))
+typealias EdgeMask Vector{Vector{Bool}}
 
-    S = Array(Int, 0)
-    for iter in 1:k
-        fill!(node_scores, 0)
-        for run in 1:N
-            # Find the connected components after dropping each edge w.p. β
-            ccs!(g, β, ccid, sizes, visited, node_stack)
-            # Mark those connected components that intersect with the set S
-            for s in S
-                covered_by_S[ccid[s]] = true
-            end
-            # Update the scores for each vertex not in S
-            for i in 1:num_nodes(g)
-                if !covered_by_S[ccid[i]]
-                    node_scores[i] += sizes[ccid[i]]
+init_mask(g::Graph) = [Array(Bool, degree(g,i)) for i in 1:num_nodes(g)]
+
+function randomize!(m::EdgeMask, β)
+    for i in eachindex(m)
+        for j in eachindex(m[i])
+            m[i][j] = rand() < β
+        end
+    end
+end
+
+function connected_components!(g::Graph, m::EdgeMask, cc_id, cc_size,
+                               visited, node_stack)
+    for i in 1:num_nodes(g)
+        cc_id[i] = i
+        cc_size[i] = 0
+        visited[i] = false
+    end
+    # Find the connected components by doing a sequence of DFS searches
+    for i in 1:num_nodes(g)
+        if !visited[i] # We haven't already added this point to another cc
+            push!(node_stack, i)
+            while !isempty(node_stack)
+                j = pop!(node_stack)
+                if !visited[j]
+                    visited[j] = true
+                    cc_id[j] = i
+                    cc_size[i] += 1
+                    for k_ix in 1:degree(g, j)
+                        k = neighbors(g, j)[k_ix]
+                        if !visited[k] && m[j][k_ix]
+                            push!(node_stack, k)
+                        end
+                    end
                 end
             end
         end
-        best_node = indmax(node_scores)
-        best_score = maximum(node_scores) / N
-        println("Adding node $best_node, score = $best_score")
+    end
+end
+
+function maximize_influence_CWY(g::Graph, β, k; N::Int = 20000)
+    # Pre-allocate some arrays
+    cc_id = Array(Int, num_nodes(g))
+    cc_size = Array(Int, num_nodes(g))
+    visited = Array(Bool, num_nodes(g))
+    node_stack = Array(Int, 0)
+    m = init_mask(g)
+    node_score = zeros(Float64, num_nodes(g))
+    # The algorithm:
+    S = Array(Int, 0)
+    while length(S) < k
+        fill!(node_score, 0)
+        for run in 1:N
+            # Find the connected components of a random subgraph
+            randomize!(m, β)
+            connected_components!(g, m, cc_id, cc_size, visited, node_stack)
+            # Zero out contributions of connected components covered by S
+            for s in S
+                cc_size[cc_id[s]] = 0
+            end
+            # Add to the scores of each node
+            for i in 1:num_nodes(g)
+                node_score[i] += cc_size[cc_id[i]]
+            end
+        end
+        best_node = indmax(node_score)
+        best_score = maximum(node_score) / N
+        println("Adding node $best_node, score=$best_score")
         push!(S, best_node)
     end
     return S
-end
-
-function ccs!(g::Graph, β, ccid, sizes, visited, node_stack)
-    for i in 1:num_nodes(g)
-        visited[i] = false
-        ccid[i] = i
-        sizes[i] = 1
-    end
-    for i in 1:num_nodes(g)
-        if visited[i]
-            continue
-        end
-        push!(node_stack, i)
-        while !isempty(node_stack)
-            j = pop!(node_stack)
-            if visited[j]
-                continue
-            end
-            visited[j] = true
-            for k_ix in 1:degree(g, j)
-                k = neighbors(g,j)[k_ix]
-                if !visited[k] && rand() < β
-                    push!(node_stack, k)
-                    ccid[k] = i
-                    sizes[k] = 0
-                    sizes[i] += 1
-                end
-            end
-        end
-    end
 end
