de3656ac4adf855e80a8dd17404c8f0422a00cf0
Travis Dick <travis.barry.dick@gmail.com>
2016-04-20 14:45:55 -0400

    cleaned up the implementation of the acquaintance heuristic.

--- heuristics.jl
+++ heuristics.jl
@@ -8,11 +8,5 @@ end
 random_heuristic(g::Graph, k) = randperm(num_nodes(g))[1:k]
 
 function acquaintance_heuristic(g::Graph, k)
-    S = Array(Int, k)
-    for iter in 1:k
-        i = rand(1:num_nodes(g))
-        j = rand(neighbors(g, i))
-        push!(S, j)
-    end
-    return S
+    return [rand(neighbors(g, rand(1:num_nodes(g)))) for i in 1:k]
 end
